39388850a4e193133fe7b24ca39e9b14bec77f5b
Razboy20 <razboy20@gmail.com>
2019-01-21 12:23:10 -0600

    lesson10

---
+++ OOP/lesson10/computer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3.5
+print("Content-Type:text/html")
+print()
+
+import secrets
+
+import cgitb
+cgitb.enable()
+# THIS IS FOR PYTHON 3.6, SO WILL NOT WORK ON CODEWIZARDS
+
+class Keyboard():
+  def __init__(self, brand, language):
+    self.brand = brand
+    self.language = language
+    self.secret = secrets.randbelow(999999999)
+
+  def type(self, msg):
+    print(f"<p>[{self.secret}]: {msg}</p>")
+
+class Monitor():
+  def __init__(self, width, height):
+    self.size = size
+    self.resolution = resolution
+
+  def play(self, video):
+    print(f"<video src={video} controls></video>")
+
+
+class Computer():
+  def __init__(self, keyboard, screen):
+    self.os = "StoneOS"
+    self.processor = "Dirt-9000X"
+    self.keyboard = keyboard
+    self.screen = screen
+
+keyboard = Keyboard("Poop Co™", "Engliso")
+monitor = Monitor("300","300")
+
+computer = Computer(keyboiard, monitor)
+computer.keyboard.type("hajkldsfhasdlkfjhasdkljf")
+computer.screen.play("kite.ogv")
---
+++ OOP/lesson10/hw/composition.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+print("Content-Type:text/html")
+print()
+
+import cgitb
+cgitb.enable()
+
+########### H O M E W O R K ###########
+#
+# Our table is composed of multiple items, like computer, coffee mug, pen holder, books.
+# Use composition to implement this example
+#
+# - Create a separate class for Computer, Coffee Mug and Pen holder with it's attributes and methods.
+#
+# - Create a class Table and use Composition to link all the above things together.
+# 
+# - Run the methods of above classes using the Table object.
+#
+# But if you have any other example in your mind, you can implement that as well.
+#
+########################################
+
+
